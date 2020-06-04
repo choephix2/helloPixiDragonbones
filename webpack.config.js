965ebddcd323b6ee008f58fde44eeee6d1e10938
choephix2 <choephix2@gmail.com>
2020-06-04 14:29:14 +0000

    copy from the other thing

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,6 @@ module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   output: { path: buildPath },
-  module: { rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }] },
   devServer: { 
     contentBase: 'build', 
     port: 3000, 
@@ -23,7 +22,12 @@ module.exports = {
     //host: 'https://3000-d1eba775-6ecc-4b9a-a1c9-3f699d90050b.ws-eu01.gitpod.io',
     disableHostCheck: true,
   },
+  optimization: {
+    minimize: true,
+    usedExports: true,
+  },
   resolve: { extensions: ['.ts', '.js'] },
+  module: { rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }] },
   plugins: [
     new webpack.ProvidePlugin({
       PIXI: 'pixi.js' // makes dragonbones work
